d6b9d33edf6518bb92946800481fdf259727ff42
ShiftedFrequency5732 <48628117+ShiftedFrequency5732@users.noreply.github.com>
2023-09-23 18:12:36 +0200

    Draw a basic window.

---
+++ include/config.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+// Some constants.
+constexpr int INITIAL_WIDTH = 800;
+constexpr int INITIAL_HEIGHT = 800;
+constexpr const char* WINDOW_TITLE = "Path Finding Visualizer";
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+#include "../include/config.hpp"
+
 #include "../include/raylib.h"
 #include "../include/raymath.h"
 #include "../include/rlgl.h"
 
 int main() {
+    // Initialize the basic window.
+    InitWindow(INITIAL_WIDTH, INITIAL_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        // Clear the window from the previous drawings.
+        ClearBackground(BLANK);
+
+        EndDrawing();
+    }
+
     return 0;
 }
