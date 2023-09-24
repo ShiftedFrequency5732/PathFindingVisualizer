5d7b3d4d92d8af18a6453621aac3e40ce8316d64
ShiftedFrequency5732 <48628117+ShiftedFrequency5732@users.noreply.github.com>
2023-09-24 15:10:42 +0200

    Updated config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-// Some constants.
+// Window related constants.
 constexpr int INITIAL_WIDTH = 800;
 constexpr int INITIAL_HEIGHT = 800;
 constexpr const char* WINDOW_TITLE = "Path Finding Visualizer";
 
+// Grid related constants.
 constexpr int GRID_SIZE = 50;
 constexpr int MARGIN_PX = 1;
